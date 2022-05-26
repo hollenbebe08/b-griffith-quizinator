2486e2d8acf857555b49565184bf093947b0bccf
Betsy <betsy.griffith12@gmail.com>
2022-05-26 17:49:25 -0400

    added arrays for questions and answers and added an answer handler

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # JavaScript Quizinator
 
 ## Purpose 
-The JavaScript Quizinator was created to test your JavaScript knowledge. It does so by testing how many answers you can get correct to the questions in a minute.
+The JavaScript Quizinator was created to test your JavaScript knowledge by having you answer JavaScript questions in an allotted amount of time.
 
 ## JavaScript Quizinator Instructions:
+1. Click the "Start the Quiz!" button on the top of the page to begin the quiz.
+2. Answer the questions before time runs out!
+3. When you are done answering all the questions or when you run out of time (whichever comes first), your score will be displayed.
+4. Try again to beat your score!
 
 ## Website:
 
--- assets/js/quizinator.js
+++ assets/js/quizinator.js
@@ -1,38 +1,61 @@
 var startBtnEl = document.querySelector("#start");
 var questionsToAskEl= document.querySelector("#questions-to-ask");
+var formAnswerEl= document.querySelector("#answer-form");
+var answerToQuestionsEl= document.querySelector("#answer-one, #answer-two, #answer-three, #answer-four");
 // var timerEl = document.querySelector("timer");
 
 
 //Objects for Questions and Answers
-// var questions = {
-//     question1: {question:"question", answer: "answer"},
-//     question2: {question: "question", answer: "answer"},
-//     question3: {question: "question", answer: "answer"},
-//     question4: {question: "question", answer: "answer"}
-// };
+var questions = {
+    question1: "What is not a JavaScript Primitive Data Type?",
+    question2: "A JavaScript file has an extension of",
+    question3: "Which of the following type of variables is visible everywhere in your JavaScript code?",
+    question4: "which built-in method returns the calling string value converted to lower case?"
+};
+
+var answers = {
+    answer1: {answer: "Array"},
+    answer2: {answer: "js"},
+    answer3: {answer: "global variable"},
+    answer4: {answer: "toLowerCase()"}
+};
 
 //to access a question use this code, ex question1: questions.question2.answer
 //for the above code the questions is referencing the object, and then question 2 is the nested object, and answer is referencing the answer inside of that object.
 
 //questions Handler
 var questionHandler = function() {
+    //first question appears when the start button is clicked
     var questionItemEl = document.createElement("li");
     questionItemEl.className = "question-text";
-    questionItemEl.textContent = "This is the first question.";
+    questionItemEl.textContent = (JSON.stringify(questions.question1));
     questionsToAskEl.appendChild(questionItemEl);
+
+    //need the answers to appear with the question
+    
+
+    //if answer is correct, move onto next question
+        //else, window alert "that is incorrect!"
+    
+    //loop to next question
+
+    //need the timer to start and continue until it hits zero seconds or until all questions are answered
+
+    //display scores and save to localStorage
+
+    return questionHandler;
   };
 
-//Timer function
-// var timer = function() {
-//     //when the questionHandler beings so does the timer countdown
+  var answerHandler = function() {
+      //answers appear when the questionHandler starts up and correspond with each question
+      var answerItemEl= document.createElement("text");
+      answerItemEl.className= "answer-btn";
+      answerItemEl.textContent = "this is an answer";
+      answerToQuestionsEl.appendChild(answerItemEl);
 
-//     //it starts at 40 seconds and goes until it reaches zero
+  };
 
-//     //it stops when the last question's(question4) answer is clicked
-//         //else, it stops at 40 seconds and alerts "You have run out of time!" and displays the score
-
-//     timerEl.innerHTML = minutes + " : " + seconds
-// };
 
 //Event Listener to start the quiz
 startBtnEl.addEventListener("click", questionHandler);
+formAnswerEl.addEventListener("click", answerHandler);
