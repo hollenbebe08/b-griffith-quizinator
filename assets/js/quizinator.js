c7add29ff9dfede9a375eaed8cffb00355797072
Betsy <betsy.griffith12@gmail.com>
2022-05-26 22:10:59 -0400

    was able to get the question and text to display simultaneously

--- assets/js/quizinator.js
+++ assets/js/quizinator.js
@@ -1,7 +1,7 @@
 var startBtnEl = document.querySelector("#start");
 var questionsToAskEl= document.querySelector("#questions-to-ask");
 var formAnswerEl= document.querySelector("#answer-form");
-var answerToQuestionsEl= document.querySelector("#answer-one, #answer-two, #answer-three, #answer-four");
+var answerToQuestionsEl= document.querySelector("#answer-one");
 // var timerEl = document.querySelector("timer");
 
 
@@ -14,10 +14,10 @@ var questions = {
 };
 
 var answers = {
-    answer1: {answer: "Array"},
-    answer2: {answer: "js"},
-    answer3: {answer: "global variable"},
-    answer4: {answer: "toLowerCase()"}
+    answer1: {a:"Array", b:"String", c:"Boolean", d:"Number"},
+    answer2: {a:"html", b:"js", c:"jpg", d:"jsq"},
+    answer3: {a:"local variable", b:"script variable", c:"function variable", d:"global variable"},
+    answer4: {a:"tolowercase", b:"tolowercase()", c:"toLowercase()", d:"toLowerCase()"}
 };
 
 //to access a question use this code, ex question1: questions.question2.answer
@@ -31,6 +31,10 @@ var questionHandler = function() {
     questionItemEl.textContent = (JSON.stringify(questions.question1));
     questionsToAskEl.appendChild(questionItemEl);
 
+    if(questionItemEl = true) {
+        answerHandler()
+    };
+
     //need the answers to appear with the question
     
 
@@ -42,20 +46,17 @@ var questionHandler = function() {
     //need the timer to start and continue until it hits zero seconds or until all questions are answered
 
     //display scores and save to localStorage
-
-    return questionHandler;
   };
 
   var answerHandler = function() {
+      event.preventDefault();
       //answers appear when the questionHandler starts up and correspond with each question
-      var answerItemEl= document.createElement("text");
-      answerItemEl.className= "answer-btn";
-      answerItemEl.textContent = "this is an answer";
+      var answerItemEl= document.createElement("li");
+      answerItemEl.textContent = "Array";
       answerToQuestionsEl.appendChild(answerItemEl);
-
   };
 
 
 //Event Listener to start the quiz
 startBtnEl.addEventListener("click", questionHandler);
-formAnswerEl.addEventListener("click", answerHandler);
+// formAnswerEl.addEventListener("click", answerHandler);
